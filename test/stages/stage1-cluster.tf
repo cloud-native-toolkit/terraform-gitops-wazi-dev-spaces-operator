0f9338d6b9afb84b1595bc534211b1a93034ebbd
Andrew Trice <amtrice@us.ibm.com>
2022-12-14 12:02:32 -0500

    updated test case
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- test/stages/stage1-cluster.tf
+++ test/stages/stage1-cluster.tf
@@ -5,6 +5,8 @@ module "dev_cluster" {
   login_user = var.cluster_username
   login_password = var.cluster_password
   login_token = ""
+  ca_cert_file = ""
+  ca_cert = ""
 }
 
 resource null_resource output_kubeconfig {
